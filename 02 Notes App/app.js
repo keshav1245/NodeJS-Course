67a19396c6eb27155ba20b8e71693cb667b646b8
Keshav Tangri <tangri57@gmail.com>
2021-09-24 18:10:10 +0530

    Chalk Package

--- 02 Notes App/app.js
+++ 02 Notes App/app.js
@@ -9,5 +9,11 @@
 // console.log(add(5, 7));
 // console.log(mul(5, 7));
 
-const validator = require('validator')
-console.log(validator.isEmail('tangri57@gmail.com'))
+// const validator = require('validator')
+// console.log(validator.isEmail('tangri57@gmail.com'))
+
+const chalk = require('chalk');
+
+console.log(chalk.blue.bgRed.bold('Hello world!'));
+console.log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+console.log(chalk.blue.bgRed.inverse.bold('Hello world!'));
