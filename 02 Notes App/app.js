1a580c279282e12ea9b79d1b9af6a354549749aa
Keshav Tangri <tangri57@gmail.com>
2021-09-24 17:33:33 +0530

    importing and using npm validator package and updating Readme

---
+++ 02 Notes App/app.js
@@ -0,0 +1,13 @@
+// const fs = require('fs');
+// // fs.writeFileSync("notes.txt", "This file was edited by Node JS");
+
+
+// fs.appendFileSync('notes.txt', "\nMy Name is Keshav Tangri !");
+// const {name, add, mul} = require('./utils.js'); // have to give relative path !
+
+// console.log(name);
+// console.log(add(5, 7));
+// console.log(mul(5, 7));
+
+const validator = require('validator')
+console.log(validator.isEmail('tangri57@gmail.com'))
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NodeJS-Course
 Course to Learn Node JS
+
+## npm Modules learnt !
+
+1. validator : https://www.npmjs.com/package/validator
