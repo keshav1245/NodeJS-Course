2a968f797872992d91dc5a9c3e1e1d47623005ee
Keshav Tangri <tangri57@gmail.com>
2021-09-29 13:43:04 +0530

    understanding asynchronous behavious, call stack, node api,  event loops and callback queue

---
+++ 03 Weather App/app.js
@@ -0,0 +1,13 @@
+// Asynchronous basics
+// console.log('Starting')
+
+// setTimeout(()=>{
+//     console.log('Some backend call!')
+// }, 3000)
+
+// setTimeout(()=>{
+//     console.log('Some backend call part 2!')
+// }, 0)
+
+// console.log('Stopping')
+
